a7541fc840b0000fdd0039a60924c33e09dcb568
MohamedSamir309966 <152786817+MohamedSamir309966@users.noreply.github.com>
2023-12-20 10:56:25 +0200

    update config for depolyment

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+import amplifyconfig from './amplifyconfiguration.json';
 
-Amplify.configure(config);
+Amplify.configure(amplifyconfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
