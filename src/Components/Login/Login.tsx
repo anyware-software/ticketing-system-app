fda18cad85c00cbceeda045037331d8d8b661497
MohamedSamir309966 <152786817+MohamedSamir309966@users.noreply.github.com>
2023-12-21 17:51:48 +0200

    Login page done responsive

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -59,12 +59,21 @@ export default function Login() {
               "linear-gradient(to bottom, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.1)), linear-gradient(to right, rgba(0, 0, 0, 0.4)100%, rgba(0, 0, 0, 0.1))", // Adjust opacity as needed
           },
           zIndex: 0,
+          display: "flex",
+          flexDirection: "column",
         }}
       >
         <AppBar
-          position="static"
+          // position="static"
           sx={{
-            backgroundColor: "rgba(0, 0, 0, 0.1)",
+            zIndex: 1,
+            position: "relative",
+            backgroundColor: "transparent",
+            border: 0,
+            boxShadow: "none",
+            alignItems: { xs: "center", sm: "start" },
+            justifyContent: { xs: "end", sm: "start" },
+            height:'15vh'
           }}
         >
           <Toolbar disableGutters>
@@ -72,6 +81,7 @@ export default function Login() {
               sx={{
                 mx: 5,
                 mt: 3,
+                // display: { xs: "flex", sm: "flex" }
               }}
             >
               <img
@@ -83,25 +93,33 @@ export default function Login() {
           </Toolbar>
         </AppBar>
 
-        <Grid container spacing={2}>
+        <Grid container spacing={2} sx={{ overflow: "hidden", flexGrow: 1 }}>
           <Grid
             item
             xs={12}
             sm={6}
             lg={6}
-            sx={{ zIndex: 1, position: "relative" , display: "flex" , justifyContent:'center' , alignItems:"center" , height: "90vh"}}
+            sx={{
+              zIndex: 1,
+              position: "relative",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "end",
+            }}
           >
             <Box
               sx={{
                 display: "flex",
-                flexDirection: 'column',
-                gap:5
+                flexDirection: "column",
+                gap: 5,
+                width: { xs: "90%", sm: "auto" },
               }}
             >
               <Box
                 sx={{
-                  display: "flex",
+                  display: { xs: "none", sm: "flex" },
                   justifyContent: "center",
+                  xs: { display: "none" },
                 }}
               >
                 <Box
@@ -113,7 +131,7 @@ export default function Login() {
                     py: 1,
                     border: "1px solid",
                     borderColor: "#e8e8e8",
-                    px:1.5
+                    px: 1.5,
                   }}
                 >
                   <Button
@@ -146,22 +164,43 @@ export default function Login() {
                 sx={{
                   display: "flex",
                   flexDirection: "column",
+                  gap: { xs: 2, sm: 0 },
                 }}
               >
-                <Typography sx={{ color: "white", my: 1, fontWeight: "700" }}>
+                <Typography
+                  sx={{
+                    color: "white",
+                    my: 1,
+                    fontWeight: "700",
+                    display: { xs: "flex", sm: "none" },
+                  }}
+                >
+                  Sign in
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "white",
+                    my: 1,
+                    fontWeight: "700",
+                    display: { xs: "none", sm: "flex" },
+                  }}
+                >
                   User name
                 </Typography>
                 <TextField
                   id="username"
                   placeholder="Type your Username"
                   sx={{
-                    backgroundColor: "#625d5b",
+                    backgroundColor: "rgba(255, 255, 255, 0.31)",
                     "input::placeholder": {
                       color: "white",
                     },
                     input: {
                       color: "white",
                     },
+                    border: "1px solid",
+                    borderColor: "rgba(255, 255, 255, 0.63)",
+                    width: "100%",
                   }}
                 />
                 <Typography
@@ -170,6 +209,7 @@ export default function Login() {
                     my: 1,
                     fontWeight: "700",
                     marginTop: "3vh",
+                    display: { xs: "none", sm: "flex" },
                   }}
                 >
                   Password
@@ -182,7 +222,9 @@ export default function Login() {
                         color: "white",
                       },
                       color: "white",
-                      backgroundColor: "#625d5b",
+                      backgroundColor: "rgba(255, 255, 255, 0.31)",
+                      border: "1px solid",
+                      borderColor: "rgba(255, 255, 255, 0.63)",
                     }}
                     type={showPassword ? "text" : "password"}
                     endAdornment={
@@ -272,7 +314,6 @@ export default function Login() {
                 sx={{
                   display: "flex",
                   justifyContent: "center",
-                  
                 }}
               >
                 <Typography
@@ -288,6 +329,7 @@ export default function Login() {
                   display: "flex",
                   justifyContent: "center",
                   gap: 2,
+                  flexDirection: { xs: "column", sm: "row" },
                 }}
               >
                 <Box
@@ -321,10 +363,9 @@ export default function Login() {
                         color: "white",
                       }}
                     >
-                      Login with instagram
+                      Login with Instagram
                     </Typography>
                   </Box>
-                  <Box></Box>
                 </Box>
                 <Box
                   sx={{
@@ -372,7 +413,7 @@ export default function Login() {
             sx={{
               zIndex: 1,
               position: "relative",
-              display: "flex",
+              display: { xs: "none", sm: "flex" },
               // flexDirection: "column",
               alignItems: "center",
               justifyContent: "center",
@@ -382,7 +423,7 @@ export default function Login() {
               sx={{
                 display: "flex",
                 flexDirection: "column",
-                marginLeft:'10rem'
+                marginLeft: "10rem",
               }}
             >
               <img
@@ -410,13 +451,51 @@ export default function Login() {
             sx={{
               zIndex: 1,
               position: "relative",
-              display: "flex",
+              display: { xs: "none", sm: "flex" },
+              // flexDirection: "column",
+              alignItems: "end",
+              justifyContent: "center",
+            }}
+          >
+            <img src="../../../Images/anyware.png" alt="" />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            lg={12}
+            sx={{
+              zIndex: 1,
+              position: "relative",
+              display: { xs: "flex", sm: "none" },
               // flexDirection: "column",
               alignItems: "center",
               justifyContent: "center",
             }}
           >
-            <img src="../../../Images/anyware.png" alt="" />
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: "15",
+                fontWeight: "400",
+                lineHeight: "25px",
+                wordWrap: "break-word",
+              }}
+            >
+              Don't have an account?
+              <Button
+                variant="text"
+                sx={{
+                  color: "#F2583E",
+                  fontSize: "15",
+                  fontWeight: "400",
+                  lineHeight: "25px",
+                  wordWrap: "break-word",
+                }}
+              >
+                Register
+              </Button>
+            </Typography>
           </Grid>
         </Grid>
       </Box>
