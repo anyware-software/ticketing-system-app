637901b563c53d58b48f9937ab95343e9a92a15c
MohamedSamir309966 <152786817+MohamedSamir309966@users.noreply.github.com>
2023-12-26 21:08:52 +0200

    update service done

---
+++ src/services/updateGuest.ts
@@ -0,0 +1,48 @@
+import { EndPoints} from "../constants/Enums";
+import axios from "axios";
+
+async function updateUser(
+    userID:any,
+    email?:any,
+    name?:any,
+    phone_number?:any,
+    birthdate?: any,
+    gender?: any,
+    guest_avatar?: any,
+    deleted?:any,
+    createdAt?:any,
+) {
+  try {
+    const operationId = 3;
+    const userAttributes={
+        email:email,
+        name:name,
+        phone_number:phone_number,
+        birthdate: birthdate,
+        gender: gender,
+        guest_avatar: guest_avatar,
+        deleted:deleted,
+        createdAt:createdAt,
+      }
+    
+    const requestBody = {
+      operationId,
+      userID,
+      userAttributes:userAttributes,
+    };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    };
+    const response = await axios.post(EndPoints.getData, requestBody);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating user :", error);
+    throw error;
+  }
+}
+
+export default updateUser;
