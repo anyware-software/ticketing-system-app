fd84597200452edcb73a1154920a629ff9e82f63
mkhaled4053 <mkhaled@anyware.software>
2024-01-08 13:32:04 +0200

    remove aws-exports.js from .gitignore to deploy

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,10 @@ import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
 import { Provider } from "react-redux";
 import store from "./state/store";
-import amplifyconfig from './amplifyconfiguration.json';
-// import config from "./aws-exports";
+// import amplifyconfig from './amplifyconfiguration.json';
+import config from "./aws-exports";
 
-Amplify.configure(amplifyconfig);
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
