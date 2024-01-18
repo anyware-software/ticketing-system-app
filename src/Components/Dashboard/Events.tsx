ee7224914e1b872a7652a8a15ecd2ea1eae3a4b1
MohamedSamir309966 <152786817+MohamedSamir309966@users.noreply.github.com>
2024-01-18 12:42:43 +0200

    base events list

--- src/Components/Dashboard/Events.tsx
+++ src/Components/Dashboard/Events.tsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import listEvents from "../../services/listEvents";
+import { dbStorage } from "../../constants/Enums";
+import ContentLoader from "../ContentLoader/ContentLoder";
+import Typography from "@mui/material/Typography";
+
+interface Event {
+  id: string;
+  name: string;
+  image: string;
+  gallery: [string];
+}
 
 export default function Events() {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const getListEvents = async () => {
+    let events = await listEvents();
+    // console.log(events.items);
+    setEvents(events.items);
+  };
+
+  useEffect(() => {
+    getListEvents();
+  }, []);
+
+  useEffect(() => {
+    if (events.length < 1) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [events]);
+
+  if (loading)
+    return (
+      <Box
+        sx={{
+          width: "100%",
+        }}
+      >
+        <ContentLoader />
+      </Box>
+    );
+
   return (
     <>
       <Box
@@ -34,7 +77,15 @@ export default function Events() {
           position: "relative",
         }}
       >
-        <Grid container spacing={2}>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            ml: 15,
+            mt: 12,
+            overflow: "auto",
+          }}
+        >
           <Grid
             item
             xs={12}
@@ -45,7 +96,56 @@ export default function Events() {
               position: "relative",
             }}
           >
-            <Box></Box>
+            {events.map((event) => (
+              <Box>
+                <Box
+                  key={event.id}
+                  sx={{
+                    display: "flex",
+                    gap:7,
+                  }}
+                >
+                  <Box>
+                  <img
+                    src={
+                      event.image
+                        ? `${dbStorage}${event.image}`
+                        : "../../../Images/event.png"
+                    }
+                    alt={event.name}
+                    style={{ width: "15rem", height: "15rem" , borderRadius:'10px'}}
+                  />
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <Box sx={{
+                      display: "flex",
+                      gap:1
+                    }}>
+                      {event?.gallery?.map((image, index) => (
+                        <img
+                          key={index}
+                          src={
+                            image
+                              ? `${dbStorage}${image}`
+                              : "../../../Images/event.png"
+                          }
+                          alt={`Event ${index + 1}`}
+                          style={{ width: "8rem", height: "5rem" , borderRadius:'5px' }}
+                        />
+                      ))}
+                    </Box>
+                    <Typography sx={{ color: "white" }}>
+                      {event.name}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+            ))}
           </Grid>
         </Grid>
       </Box>
