7875fc079ed6bf3bca8f9dde34c842be7c12087f
MohamedSamir309966 <152786817+MohamedSamir309966@users.noreply.github.com>
2024-01-28 11:22:14 +0200

    service createBooking

---
+++ src/services/createBooking.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { EventEndPoints } from "../constants/Enums";
+
+async function createBooking(
+  user: any,
+  status: string,
+  bookingMainGuestId: string,
+  bookingGuestId: string,
+  bookingEventId: string,
+  bookingEventTicketId: string,
+  isMainGuest: Boolean,
+  wave: string,
+  orderId: String,
+  specialNeed: Boolean,
+) {
+  try {
+    const operationId = 3;
+    const bookAttributes = {
+      status: status,
+      bookingMainGuestId: bookingMainGuestId,
+      bookingGuestId: bookingGuestId,
+      bookingEventId: bookingEventId,
+      bookingEventTicketId: bookingEventTicketId,
+      isMainGuest: isMainGuest,
+      wave: wave,
+      orderId: orderId,
+      specialNeed: specialNeed,
+      createdAt: new Date(),
+      createdByID: user.sub,
+      createdByName: user.email,
+    }
+
+    const requestBody = {
+      operationId,
+      bookAttributes: bookAttributes
+    };
+    console.log(requestBody)
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    };
+    const response = await axios.post(EventEndPoints, requestBody);
+    return response.data;
+
+  } catch (error) {
+    console.error("Error creating user :", error);
+    throw error;
+  }
+}
+
+export default createBooking;
