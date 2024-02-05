9f8211357d091fa90b393f3da9d3ba9cb7349ec8
MohamedSamir309966 <152786817+MohamedSamir309966@users.noreply.github.com>
2024-02-05 10:16:56 +0200

    fixed render booking issue

--- src/Components/Dashboard/GuestProfile.tsx
+++ src/Components/Dashboard/GuestProfile.tsx
@@ -1507,7 +1507,7 @@ export default function GuestProfile() {
             mx: { xs: 0, sm: 3, md: 0, lg: 0 },
           }}
         >
-          {currentBookings && currentCompanions.length > 0 ? (
+          {currentBookings ? (
             <Box
               sx={{
                 display: "flex",
@@ -1982,7 +1982,7 @@ export default function GuestProfile() {
                         display: { xs: "none", sm: "block", lg: "block" },
                       }}
                       onClick={() => {
-                        navigate("/dashboard/");
+                        navigate("/dashboard/events/");
                       }}
                     >
                       Book Now !
