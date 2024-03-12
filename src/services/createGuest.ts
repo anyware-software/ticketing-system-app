3cf2cce7646d7addaf4b094b2af990b0d565e69e
MohamedSamir309966 <152786817+MohamedSamir309966@users.noreply.github.com>
2024-03-12 15:25:32 +0200

    sure that guest name and email are small

--- src/services/createGuest.ts
+++ src/services/createGuest.ts
@@ -1,34 +1,36 @@
 import axios from "axios";
-import { EndPoints} from "../constants/Enums";
+import { EndPoints } from "../constants/Enums";
 
 async function createGuest(
-    user:any,
-    group:any,
-    faceBookID:any,
-    gender:any,
+  user: any,
+  group: any,
+  faceBookID: any,
+  gender: any
 ) {
   // console.log(user,group,faceBookID);
-  
-  try {    
+
+  try {
     const operationId = 1;
-    const userAttributes={
-      email:user.email,
-      name:user.name?user.name:"user",
-      phone_number:user.phone_number?(user.phone_number.substring(2,user?.phone_number.length+1)):"+20",
-      group:group,
-      faceBookID:faceBookID,
-      gender:gender,
+    const userAttributes = {
+      email: user.email.toLowerCase(),
+      name: user.name ? user.name.toLowerCase() : "user",
+      phone_number: user.phone_number
+        ? user.phone_number.substring(2, user?.phone_number.length + 1)
+        : "+20",
+      group: group,
+      faceBookID: faceBookID,
+      gender: gender,
       createdAt: new Date().toISOString(),
-      createdByID:user.sub,
+      createdByID: user.sub,
       createdByName: user.email,
-    }
-    
+    };
+
     const requestBody = {
       operationId,
-      userID:user.sub,
-      userAttributes:userAttributes
+      userID: user.sub,
+      userAttributes: userAttributes,
     };
-    console.log(requestBody)
+    console.log(requestBody);
     const options = {
       method: "POST",
       headers: {
@@ -36,9 +38,8 @@ async function createGuest(
       },
       body: JSON.stringify(requestBody),
     };
-    const response = await axios.post( EndPoints.getData , requestBody );
+    const response = await axios.post(EndPoints.getData, requestBody);
     return response.data;
-
   } catch (error) {
     console.error("Error creating user :", error);
     throw error;
